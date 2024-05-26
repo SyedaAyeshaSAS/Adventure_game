f8264e173bdc5216300bae52144126c68bfb8d85
SyedaAyeshaSAS <160591813+SyedaAyeshaSAS@users.noreply.github.com>
2024-05-26 20:20:58 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,99 @@
+#! usr/bin/env node
+
+
+
+import chalk from "chalk";
+import inquirer from "inquirer";
+
+class Player {
+  name: string;
+  fuel: number = 100;
+  constructor(name: string) {
+    this.name = name;
+  }
+  fuelDecrease() {
+    let fuel = this.fuel - 35;
+    this.fuel = fuel;
+  }
+  fuelIncrease() {
+    let fuel = this.fuel + 25;
+    this.fuel = fuel;
+  }
+}
+
+class Opponent {
+  name: string;
+  fuel: number = 100;
+  constructor(name: string) {
+    this.name = name;
+  }
+  fuelDecrease() {
+    let fuel = this.fuel - 35;
+    this.fuel = fuel;
+  }
+}
+
+let player = await inquirer.prompt({
+  type: "input",
+  name: "name",
+  message: "Enter your name :",
+});
+let opponent = await inquirer.prompt({
+  type: "list",
+  name: "select",
+  message: "Select your opponent :",
+  choices: ["Skeleton", "batman", "Zombie"],
+});
+console.log(
+  `${chalk.bold.blue(player.name)} VS ${chalk.bold.blue(opponent.select)}`
+);
+
+let p1 = new Player(player.name);
+let o1 = new Opponent(opponent.select);
+do {
+  if (
+    opponent.select === "Skeleton" ||
+    opponent.select === "batman" ||
+    opponent.select === "Zombie"
+  ) {
+    let ask = await inquirer.prompt({
+      type: "list",
+      name: "option",
+      message: "Select your option :",
+      choices: ["Attack", "Health drink", "Run for life..."],
+    });
+    if (ask.option === "Attack") {
+      let number = Math.floor(Math.random() * 2);
+      if (number <= 0) {
+        p1.fuelDecrease();
+        console.log(`${p1.name}'s fuel is ${chalk.bold.green(p1.fuel)}`);
+        console.log(`${o1.name}'s fuel is ${chalk.bold.green(o1.fuel)}`);
+
+        if (p1.fuel <= 0) {
+          console.log(
+            chalk.bold.blue(`${p1.name} Lose! Better luck next time.`)
+          );
+          process.exit();
+        }
+      }
+      if (number > 0) {
+        o1.fuelDecrease();
+        console.log(`${p1.name}'s fuel is ${chalk.bold.green(p1.fuel)}`);
+        console.log(`${o1.name}'s fuel is ${chalk.bold.green(o1.fuel)}`);
+
+        if (o1.fuel <= 0) {
+          console.log(chalk.bold.green(`Congratulations ${p1.name} You Win.`));
+          process.exit();
+        }
+      }
+    }
+    if (ask.option === "Health drink") {
+      p1.fuelIncrease();
+      console.log(`${p1.name}'s fuel is ${chalk.bold.green(p1.fuel)}`);
+    }
+    if (ask.option === "Run for life...") {
+      console.log(chalk.bold.green(`${p1.name} Lose! Better luck next time.`));
+      process.exit();
+    }
+  }
+} while (true);
